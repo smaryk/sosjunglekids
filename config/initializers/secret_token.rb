666edc5d4598d93f13f60c2a0262ca55e85005b0
Sabla <smkerketta@gmail.com>
2014-07-20 18:31:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sosjunglekids::Application.config.secret_key_base = 'be3b36f74edee6daf070d4d8052b3fe5d74b131af3ef05a2c4bd86f76f280bdfc1f96da2966cfab48c2d966a3a395fddb455b35bc095b826657bd119072b5603'
